0f69845cc2a3be08e6a8d8d2c3601f20ddaee2a9
zhangtong <289649077@qq.com>
2020-11-27 22:50:02 +0800

    Add types

--- packages/apps-config/src/api/spec/stafi.ts
+++ packages/apps-config/src/api/spec/stafi.ts
@@ -8,5 +8,14 @@ export default {
   RefCount: 'u32',
   ChainId: 'u8',
   ResourceId: '[u8; 32]',
-  DepositNonce: 'u64'
+  DepositNonce: 'u64',
+  RateType: 'u64',
+  AccountRData: {
+    free: 'u128'
+  },
+  RSymbol: {
+    _enum: [
+      'RFIS'
+    ]
+  }
 };
